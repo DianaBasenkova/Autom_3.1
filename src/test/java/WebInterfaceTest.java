d0da2b609845695239b1553ccb6b861568aeb3e1
Diana Basenkova <dinnight123@yandex.ru>
2023-05-03 20:16:09 +0300

    One test fallen

--- src/test/java/WebInterfaceTest.java
+++ src/test/java/WebInterfaceTest.java
@@ -1,27 +1,30 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-public class WebInterfaceTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class ChromeSeleniumTest{
     private WebDriver driver;
+
     @BeforeAll
-    static void setUpAll(){
-        System.setProperty("webdriver.chrome.driver", "driver/windows/chromedriver");
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
     }
-
-    @BeforeEach
-    void setUp() {
-        driver = new ChromeDriver();
-        /*ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);*/
-    }
-
+        @BeforeEach
+        void setUp() {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+            driver.get("http://localhost:9999/");
+        }
     @AfterEach
     void tearDown() {
         driver.quit();
@@ -30,7 +33,12 @@ public class WebInterfaceTest {
 
     @Test
     void shouldFillByValidData() {
-        driver.get("http://localhost:9999");
-
+        driver.findElement(By.xpath("//span[@data-test-id='name']//input")).sendKeys("Иванов Иван");
+        driver.findElement(By.xpath("//span[@data-test-id='phone']//input")).sendKeys("+79123456789");
+        driver.findElement(By.xpath(".//span[contains(@class, 'checkbox__box')]")).click();
+        driver.findElement(By.xpath(".//span[contains(text(), 'Продолжить')]")).click();
+        String actual = driver.findElement(By.xpath("//div[contains(@class, 'Success_successBlock')]//p")).getText();
+        String expected = " Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actual);
     }
-}
+    }
